3ddfea0781cfeca0c39be3bf244a66b0fa799be1
Ninh <hoangninh2106@gmail.com>
2025-12-20 10:10:14 +0700

    coupon manager page(import CoupomManagerPage)

--- src/features/promotion/PromotionRoutes.tsx
+++ src/features/promotion/PromotionRoutes.tsx
@@ -5,6 +5,7 @@ import AuthProtector from '@/components/container/AuthProtector'
 import PermissionProtector from '@/components/container/PermissionProtector'
 import permissions from '@/configs/permissions'
 import PromotionManagementPage from '@/features/promotion/pages/PromotionManagementPage'
+import CouponManagementPage from '@/features/promotion/pages/CouponManagementPage'
 
 const PromotionRoutes = [
     {
@@ -19,6 +20,15 @@ const PromotionRoutes = [
             {
                 path: '',
                 element: <PromotionManagementPage />
+            },
+            {
+                path: 'phieu-giam-gia',
+                element: (
+                    <PermissionProtector
+                        children={<CouponManagementPage />}
+                        permission={permissions.accessCouponDashboardPage}
+                    />
+                )
             }
         ]
     }
